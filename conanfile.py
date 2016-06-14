5b1a079dcf6fa9542f74aae54a51bbc130dad32d
memsharded <james@conan.io>
2016-06-15 02:27:07 +1000

    test_package working

--- conanfile.py
+++ conanfile.py
@@ -2,20 +2,19 @@ from os.path import join as path_join
 from conans import ConanFile, CMake
 
 class RapidJSONConan(ConanFile):
-    name = 'RapidJSON'
+    name = "RapidJSON"
     version = '1.0.2'
     url = 'https://github.com/SamuelMarks/conan-rapidjson'
     license = 'SQLite'
-    exports = 'FindRapidjson.cmake' #, '*rapidjson.h'
-    settings = None
-    options = None # {'header_only': [True]}
+    exports = 'FindRapidjson.cmake'
+
+    def source(self):
+        self.run("git clone https://github.com/miloyip/rapidjson")
+        self.run("cd rapidjson && git checkout v%s" % self.version)
 
     def package(self):
         self.copy('FindRapidjson.cmake', '.', '.')
-        #self.copy(src='*rapidjson.h', dst='.', keep_path=False)
-        self.copy('*', dst='include', src='rapidjson/include/rapidjson')
+        self.copy('*', dst='include', src='rapidjson/include')
 
-    def build(self): pass
-
-    def package_info(self):
-        self.cpp_info.libs = ['rapidjson']
+    def build(self):
+        pass
